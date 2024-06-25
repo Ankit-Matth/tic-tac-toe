ad5d7b1b6b0c9ee0983d32d8146e7e69045d959e
Ankit-Matth <ankitmatth111@gmail.com>
2024-06-25 14:55:57 +0530

    adding ssl certificate with letsencrypt

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -19,7 +19,7 @@ import io from 'socket.io-client';
 Cypress.Commands.add('createOpponentConnection', (roomCode) => {
     cy.wrap(
         new Promise((resolve) => {
-            const opponentSocket = io('http://43.205.103.103:5345/');
+            const opponentSocket = io('https://tic-tac-toe-server.linkpc.net/');
             opponentSocket.on('connect', () => {
                 const opponentData = {
                     name: 'Amit Matth',
